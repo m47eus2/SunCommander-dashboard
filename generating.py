9ca6f5af577d625dd31e96b5aa02c817cfa9e77d
m47eus2 <mateusz.ciesielski2003@gmail.com>
2025-07-17 10:02:18 +0200

    Generating new column in csv - cumulatedEnergy

--- generating.py
+++ generating.py
@@ -1,22 +1,33 @@
 import csv
 import os
-import datetime
 import random
 from datetime import datetime
 import time
+import pandas as pd
 
 PATH = "data.csv"
+
+def getCEnergyValue():
+    if os.path.isfile(PATH):
+        data = pd.read_csv(PATH)
+        data = data.tail(1)
+        return int(data['cEnergy'].values[0])
+    else:
+        return 0
+
 
 def log(now,prod,ra,rb,rc,b1,b2,energy):
     fileExists = os.path.isfile(PATH)
     with open(PATH, 'a', newline='') as file:
         writer = csv.writer(file)
         if not fileExists:
-            writer.writerow(['time','prod','ra','rb','rc','b1','b2','energy'])
+            writer.writerow(['time','prod','ra','rb','rc','b1','b2','energy','cEnergy'])
             print("First row created")
-        writer.writerow([now,prod,ra,rb,rc,b1,b2,energy])
+        cEnergy["value"] += energy
+        writer.writerow([now,prod,ra,rb,rc,b1,b2,energy,cEnergy["value"]])
         print(f"Data logged -> {now}")
 
+cEnergy = {"value":getCEnergyValue()}
 
 while(True):
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
