f6656252ae8d2cd970d2729b3ea96adc65da8450
m47eus2 <mateusz.ciesielski2003@gmail.com>
2025-07-31 14:06:31 +0200

    Adding logging to agregator

--- agregator.py
+++ agregator.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime, timedelta
 
 yesterday = datetime.now() - timedelta(days=1)
-PATH = f"database/{yesterday.strftime('%Y-%m-%d')}-log.csv"
+PATH = f"/home/raspberrypi/pythonBokeh/v3/database/{yesterday.strftime('%Y-%m-%d')}-log.csv"
 
 data = pd.read_csv(PATH)
 data['time'] = pd.to_datetime(data['time'], format="%Y-%m-%d %H:%M:%S")
@@ -21,11 +21,15 @@ for hour in range(24):
         avrs.append(avr if not pd.isnull(avr) else 0)
     avrRows.append(avrs)
 
-AGRPATH = "database/agrData.csv"
+AGRPATH = "/home/raspberrypi/pythonBokeh/v3/database/agrData.csv"
 
 fileExists = os.path.isfile(AGRPATH)
 with open(AGRPATH, 'a', newline='') as file:
     writer = csv.writer(file)
     if not fileExists:
         writer.writerow(data.columns.tolist())
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{now}]: Created new agrData.csv file")
     writer.writerows(avrRows)
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{now}]: Agregated data from {PATH}")
